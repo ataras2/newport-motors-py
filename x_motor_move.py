ab6b8c6e67d982dba823969ee3b1493c1cfd8bff
ataras2 <ataras2@gmail.com>
2023-04-26 09:52:52 +1000

    init commit with some simple motor tests

---
+++ x_motor_move.py
@@ -0,0 +1,17 @@
+#!/home/adam/miniconda3/envs/motors/bin/python python
+
+import pyvisa
+
+
+rm = pyvisa.ResourceManager()
+print(rm.list_resources())
+# exit()
+inst = rm.open_resource('ASRL/dev/ttyUSB3::INSTR',
+                        baud_rate=921600, 
+                        write_termination='\r\n',
+                        read_termination='\r\n')
+# pos = inst.query('01PA?').strip()
+# print(pos)
+
+pos = inst.query('1TPU').strip()
+print(pos)
