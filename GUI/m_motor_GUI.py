10a32f31e46c8ac9bedd51ecc1ec7a709cd8cb74
ataras2 <ataras2@gmail.com>
2023-04-26 11:01:32 +1000

    added simple button and state updater

--- GUI/m_motor_GUI.py
+++ GUI/m_motor_GUI.py
@@ -1,24 +1,49 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, State
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+sub_asms = ["OAP1", "spherical_mirror", "knife_edge"]
+configs = list(range(1,5))
+
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    html.H1(children='Motor Control GUI', style={'textAlign':'center'}),
+    html.H3(children='Adam Taras: adam.taras@sydney.edu.au', style={'textAlign':'left'}),
+    dcc.Dropdown(
+        sub_asms, 
+        id='subasm-selection',
+        searchable = False,
+        clearable = False,
+        placeholder="Select a sub assembly"
+    ),
+    dcc.Dropdown(
+        configs, 
+        id='config-selection',
+        searchable = False,
+        clearable = False,
+        placeholder="Select a config"
+    ),
+    html.Button('Center', id='center-button', n_clicks=0),
+    html.Div(id='text-placeholder',
+             children='Hit the buttons :)')
+    # dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    # dcc.Graph(id='graph-content')
 ])
 
+def get_motor_map(subasm, config):
+    return "/dev/ttyUSBx"
+
 @callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Output('text-placeholder', 'children'),
+    State('subasm-selection', 'value'),
+    State('config-selection', 'value'),
+    Input('center-button', 'n_clicks')
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+def update_motor(subasm, config, n_clicks):
+    return f"Sending Center command to {get_motor_map(subasm, config)} a.k.a {subasm}, {config}"
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
