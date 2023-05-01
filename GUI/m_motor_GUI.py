2e9977fb0d38a3c377ad1fb061fb6fc2c3c71f5f
ataras2 <ataras2@gmail.com>
2023-05-01 18:03:52 +1000

    started motor gui again

--- GUI/m_motor_GUI.py
+++ GUI/m_motor_GUI.py
@@ -1,49 +1,24 @@
-from dash import Dash, html, dcc, callback, Output, Input, State
-import plotly.express as px
+import streamlit as st
 import pandas as pd
-
-sub_asms = ["OAP1", "spherical_mirror", "knife_edge"]
-configs = list(range(1,5))
+import numpy as np
 
 
-app = Dash(__name__)
+st.title('Motor control for Heimdallr')
 
-app.layout = html.Div([
-    html.H1(children='Motor Control GUI', style={'textAlign':'center'}),
-    html.H3(children='Adam Taras: adam.taras@sydney.edu.au', style={'textAlign':'left'}),
-    dcc.Dropdown(
-        sub_asms, 
-        id='subasm-selection',
-        searchable = False,
-        clearable = False,
-        placeholder="Select a sub assembly"
-    ),
-    dcc.Dropdown(
-        configs, 
-        id='config-selection',
-        searchable = False,
-        clearable = False,
-        placeholder="Select a config"
-    ),
-    html.Button('Center', id='center-button', n_clicks=0),
-    html.Div(id='text-placeholder',
-             children='Hit the buttons :)')
-    # dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    # dcc.Graph(id='graph-content')
-])
-
-def get_motor_map(subasm, config):
-    return "/dev/ttyUSBx"
-
-@callback(
-    Output('text-placeholder', 'children'),
-    State('subasm-selection', 'value'),
-    State('config-selection', 'value'),
-    Input('center-button', 'n_clicks')
-)
-def update_motor(subasm, config, n_clicks):
-    return f"Sending Center command to {get_motor_map(subasm, config)} a.k.a {subasm}, {config}"
+# error here, on change uses the previous value of component or beam 
+# i.e. the call back occurs before beam assignment...
+def on_item_change():
+   print(f"Changing internals to {component}{beam}")
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+col1, col2 = st.columns(2)
+
+with col1:
+   component = st.selectbox("Pick a component", ["OAP1","Spherical mirror"], on_change=on_item_change)
+
+with col2:
+   beam = st.selectbox("Pick a component", list(range(1,5)), on_change=on_item_change)
+
+st.write(f"Currently looking at {component}, beam {beam}")
+
+
