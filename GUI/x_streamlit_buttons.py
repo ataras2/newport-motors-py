f18800124e8c8afa43fac9e881b3ca2e4e019dd8
Adam Taras <ataras2@gmail.com>
2023-05-03 09:52:07 +1000

    added comment

--- GUI/x_streamlit_buttons.py
+++ GUI/x_streamlit_buttons.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
 if 'x' not in st.session_state:
+    # run this only the first time the app is opened
     st.session_state['x'] = 0
 
-print("before:", st.session_state)
 
 inc = st.number_input('Step size', value=0, key='increment', step=1)
-
-print("after:", st.session_state)
 
 x = st.number_input('x', value=st.session_state['x'], key='x', step = inc)
 
