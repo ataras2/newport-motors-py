7ab2a402de09d96bf42bfd4508ef63de2151538c
ataras2 <ataras2@gmail.com>
2023-05-09 13:58:01 +1000

    further improved on MWE with less functions

--- examples/x_update_values_persistence.py
+++ examples/x_update_values_persistence.py
@@ -12,27 +12,24 @@ st.set_page_config(layout="wide")
 
 st.title('Testing ways of making the values of numerics update correctly')
 
-if 'x' not in st.session_state:
-    st.session_state['x'] = 0.
 
-def select_new_object():
-    print(st.session_state['component'])
-    st.session_state['x'] = st.session_state['current_values'][st.session_state['component']]
     
 
 component = st.selectbox("Pick a component", 
                          ["OAP1","Spherical mirror","Knife edge"], 
-                         key="component", 
-                         on_change=select_new_object)
+                         key="component")
 
 
 def update_cur_values():
+    """
+    updates the storage of current values. In the full system, this is handled by the motor class
+    """
     st.session_state['current_values'][st.session_state['component']] = st.session_state['x']
     print(st.session_state['current_values'])
 
 
 st.number_input('x', 
-                value=st.session_state['x'], 
+                value=st.session_state['current_values'][st.session_state['component']], 
                 min_value=-0.75, 
                 max_value=0.75, 
                 key='x', 
