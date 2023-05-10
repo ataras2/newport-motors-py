899074d24ea4937371f13f75e61597bf9c965821
ataras2 <ataras2@gmail.com>
2023-05-10 14:42:32 +1000

    simple mock testing

---
+++ tests/Mockers.py
@@ -0,0 +1,30 @@
+from visa_mock.base.base_mocker import BaseMocker, scpi
+
+class MotorMocker(BaseMocker):
+    """
+    The main mocker class. 
+    """
+
+    def __init__(self, call_delay: float = 0.0):
+        super().__init__(call_delay=call_delay)
+
+
+
+class Mock_M100D(MotorMocker):
+    """
+    The mocker for the tip/tilt motor
+    """
+
+    def __init__(self, call_delay: float = 0.0):
+        super().__init__(call_delay=call_delay)
+
+    @scpi("*IDN?")
+    def idn(self) -> str: 
+        """
+        'vendor', 'model', 'serial', 'firmware'
+        """
+        return "Mocker,testing,00000,0.01"
+    
+    @scpi("<address>TP<axis>")
+    def get_abs_pos(self, address : int, axis : str) -> str: 
+        return 0.01
--- tests/t_pyvisa_mock.py
+++ tests/t_pyvisa_mock.py
@@ -3,26 +3,6 @@ from pyvisa import ResourceManager
 from visa_mock.base.base_mocker import BaseMocker, scpi
 from visa_mock.base.register import register_resource
 
-class MockerChannel(BaseMocker): 
-    """
-    A mocker channel for a multi channel voltage source. 
-    Voltages are zero by default
-    """
-    
-    def __init__(self, call_delay: float = 0.0):
-        super().__init__(call_delay=call_delay)
-        self._voltage = 0
-    
-    # Lets define handler functions. 
-    
-    @scpi(":VOLTage <voltage>") 
-    def _set_voltage(self, voltage: float) -> None:
-        self._voltage = voltage
-    
-    @scpi(":VOLTage?")
-    def _get_voltage(self) -> float: 
-        return self._voltage
-
 
 class Mocker(BaseMocker):
     """
@@ -31,7 +11,6 @@ class Mocker(BaseMocker):
 
     def __init__(self, call_delay: float = 0.0):
         super().__init__(call_delay=call_delay)
-        self._channels = defaultdict(MockerChannel)
 
     @scpi("*IDN?")
     def idn(self) -> str: 
@@ -40,16 +19,22 @@ class Mocker(BaseMocker):
         """
         return "Mocker,testing,00000,0.01"
     
-    @scpi(":INSTRument:CHAnnel<channel>")
-    def _get_channel(self, channel: int) -> MockerChannel:
-        return self._channels[channel] 
-        
+    @scpi("<address>TP<axis>")
+    def idn(self, address : int, axis : str) -> str: 
+        """
+        'vendor', 'model', 'serial', 'firmware'
+        """
+        return axis
+
+
+
 register_resource("MOCK0::mock1::INSTR", Mocker())
 
 rc = ResourceManager(visa_library="@mock")
 res = rc.open_resource("MOCK0::mock1::INSTR")
-res.write(":INSTR:CHANNEL1:VOLT 2.3")
-reply = res.query(":INSTR:CHA1:VOLT?")  # This should return '2.3'
-reply = res.query(":instrument:channel1:voltage?") # We can either use the short form or the long form
-
+reply = res.query("*IDN?")  
 print(reply)
+reply = res.query("1TPU")  
+print(reply)
+# reply = res.query("*?IDN?")  
+# print(reply)
