8f83c14eda45edf57c7614278fe11999633cfc30
ataras2 <ataras2@gmail.com>
2023-05-11 15:00:23 +1000

    added linear motor driver code

--- newport_motors/Motors/motor.py
+++ newport_motors/Motors/motor.py
@@ -26,6 +26,10 @@ class Motor:
 
 
 class M100D(Motor):
+    """
+    A tip tilt motor driver class
+    https://www.newport.com.cn/p/CONEX-AG-M100D
+    """
     AXES = Enum('AXES', ['U', 'V'])
     HW_BOUNDS = {
         AXES.U : [-0.75, 0.75],
@@ -40,6 +44,9 @@ class M100D(Motor):
         }
 
     def _verify_valid_connection(self):
+        """
+        Verify that the serial connection opened by the class is indeed to to a NEWPORT M100D
+        """
         id = self._connection.query('1ID?').strip()
         assert 'M100D' in id
     
@@ -64,7 +71,7 @@ class M100D(Motor):
             position (float) : the position of the axis in degrees
         """
         return_str = self._connection.query(f'1TP{axis.name}').strip()
-        return float(parse.parse('{}TPU{}',return_str)[1])
+        return float(parse.parse('{}'+f'TP{axis.name}'+'{}',return_str)[1])
 
     def set_absolute_position(self, value : float, axis: AXES):
         """
@@ -82,14 +89,41 @@ class M100D(Motor):
 
 
 class LS16P(Motor):
+    HW_BOUNDS = [-8., 8.]
     def __init__(self, serial_port: str, rm: pyvisa.ResourceManager):
         super().__init__(serial_port, rm)
+        self._current_pos = 0.
 
     def _verify_valid_connection(self):
+        """
+        Verify that the serial connection opened by the class is indeed to to a NEWPORT LS16P
+        """
         id = self._connection.query('1ID?').strip()
         assert 'LS16P' in id
 
+    def set_absolute_position(self, value : float):
+        """
+        Set the absolute position of the motor 
 
+        Parameters:
+            value (float) : The new position in mm
+        """
+        str_to_write = f'1PA{value}'
+        self._connection.write(str_to_write)
+        self._current_pos = value
+
+    def read_pos(self) -> float:
+        """
+        Set the absolute position of the motor 
+
+        Returns:
+            value (float) : The new position in mm
+        """
+        return_str = self._connection.query('1TP').strip()
+        return float(parse.parse('{}TP{}',return_str)[1])
+    
+    def get_current_pos(self):
+        return self._current_pos
 
 if __name__ == "__main__":
     # example code:
