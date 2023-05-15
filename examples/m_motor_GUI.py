f313b797a5cb0d137b166044a96ca311809d0ac6
ataras2 <ataras2@gmail.com>
2023-05-15 10:24:25 +1000

    added extra UI classes for each block

--- examples/m_motor_GUI.py
+++ examples/m_motor_GUI.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from newport_motors.GUI.CustomNumeric import CustomNumeric
-from newport_motors.GUI.InstrumentGUI import InstrumentGUI
-from newport_motors.Motors.motor import M100D
+from newport_motors.Motors.motor import M100D, LS16P
+from newport_motors.GUI.TipTiltUI import TipTiltUI
+from newport_motors.GUI.LinearUI import LinearUI
+
 import pyvisa
 
 # TODO: have persistence when dropdowns are changed
@@ -23,7 +24,7 @@ logging.basicConfig(
                     # filename=os.path.join('logs/', fname+".log"), 
                     encoding='utf-8', 
                     level=logging.INFO,
-                    format='%(filename)s:%(levelname)s:%(message)s')
+                    format='%(filename)s:%(levelname)s: %(message)s')
 
 run_in_sim = True
 
@@ -36,14 +37,16 @@ st.title('Motor control for Heimdallr')
 
 if 'motor1' not in st.session_state:
     if run_in_sim:
-        from newport_motors.Mocks.motor import Mock_M100D
+        from newport_motors.Mocks.motor import Mock_M100D, Mock_LS16P
         from visa_mock.base.register import register_resource
 
         motor1_port = 'MOCK0::mock1::INSTR'
         motor2_port = 'MOCK0::mock2::INSTR'
+        motor3_port = 'MOCK0::mock3::INSTR'
 
         register_resource(motor1_port, Mock_M100D())
         register_resource(motor2_port, Mock_M100D())
+        register_resource(motor3_port, Mock_LS16P())
 
         rm = pyvisa.ResourceManager(visa_library="@-mock")
     else:
@@ -54,6 +57,7 @@ if 'motor1' not in st.session_state:
 
     st.session_state['motor1'] = M100D(motor1_port, rm)
     st.session_state['motor2'] = M100D(motor2_port, rm)
+    st.session_state['motor3'] = LS16P(motor3_port, rm)
 
 
 col1, col2 = st.columns(2)
@@ -76,55 +80,14 @@ else:
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("Linear motor")
-    def update_slider():
-        st.session_state.slider = st.session_state.numeric
-    def update_numin():
-        st.session_state.numeric = st.session_state.slider            
-
-
-
-    ccol1, ccol2 = st.columns(2)
-    with ccol1:
-        val = st.number_input('Displacement (mm)', value = 0, key = 'numeric', on_change = update_slider)
-
-    with ccol2:
-        slider_value = st.slider('slider', min_value = 0, 
-                            value = val, 
-                            max_value = 5,
-                            step = 1,
-                            key = 'slider', on_change= update_numin)
+    LinearUI.main('motor3')
     
-    # st.line_chart(np.random.randn(20, 1))
-#    component = st.selectbox("Pick a component", ["OAP1","Spherical mirror"])
 
 
-def TipTiltMotor():
-    st.header("Tip/Tilt motor")
-    CustomNumeric.variable_increment(['x', 'y'],
-                                     [InstrumentGUI.get_update_fn('x', motor_key), InstrumentGUI.get_update_fn('y', motor_key)],
-                                     st.session_state[motor_key].get_current_pos())
-    pos = st.session_state[motor_key].get_current_pos()
-    logging.info(pos)
-
-    import plotly.express as px
-
-    fig = px.scatter(
-        x=np.array([pos[0]]),
-        y=np.array([pos[1]]),
-    )
-    fig.update_layout(
-        xaxis_title="x",
-        yaxis_title="y",
-        yaxis = dict(range=st.session_state[motor_key].HW_BOUNDS[M100D.AXES.V]),
-        xaxis = dict(range=st.session_state[motor_key].HW_BOUNDS[M100D.AXES.U])
-    )
-
-    st.write(fig)
 
 
 with col2:
-    TipTiltMotor()
+    TipTiltUI.main('motor1')
 #    beam = st.selectbox("Pick a component", list(range(1,5)))
 
 
---
+++ newport_motors/GUI/TipTiltUI.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import numpy as np
+
+import logging
+
+from newport_motors.GUI.CustomNumeric import CustomNumeric
+from newport_motors.GUI.InstrumentGUI import InstrumentGUI
+
+from newport_motors.Motors.motor import M100D
+
+
+class TipTiltUI:
+    def main(motor_key : str):
+        st.header("Tip/Tilt motor")
+        CustomNumeric.variable_increment(['tip_tilt_x', 'tip_tilt_y'],
+                                        [TipTiltUI.get_callback('tip_tilt_x', motor_key), 
+                                         TipTiltUI.get_callback('tip_tilt_y', motor_key)],
+                                        st.session_state[motor_key].get_current_pos())
+        pos = st.session_state[motor_key].get_current_pos()
+        logging.info(pos)
+
+        import plotly.express as px
+
+        fig = px.scatter(
+            x=np.array([pos[0]]),
+            y=np.array([pos[1]]),
+        )
+        fig.update_layout(
+            xaxis_title="x",
+            yaxis_title="y",
+            yaxis = dict(range=st.session_state[motor_key].HW_BOUNDS[M100D.AXES.V]),
+            xaxis = dict(range=st.session_state[motor_key].HW_BOUNDS[M100D.AXES.U])
+        )
+
+        st.write(fig)
+
+    def get_callback(source : str, motor_key : str) -> callable:
+        if source[-1] == 'x':
+            axis = M100D.AXES.U
+        elif source[-1] == 'y':
+            axis = M100D.AXES.V
+        else:
+            raise NotImplementedError()
+        def fn():
+            logging.info(f"sending {st.session_state[source]} to {st.session_state.component}")
+            st.session_state[motor_key].set_absolute_position(st.session_state[source], axis)
+        return fn
