2bd8c2c96a8c243d078fccc227dfc835692f7de4
ataras2 <ataras2@gmail.com>
2023-06-05 11:49:37 +1000

    linting fix

--- newport_motors/USBs/USBs.py
+++ newport_motors/USBs/USBs.py
@@ -1,17 +1,25 @@
+"""
+Module for managing USB connections
+"""
+
 import usbinfo
-from typing import Union
+
 
 class USBs:
     """
-    A calss that manages usb connections and can filter particular devices
+    A class that manages usb connections and can filter particular devices
     Useful to get the mapping serial number -> devname e.g. 12345-> /dev/ttyUSB0
     """
+
     @classmethod
     def discover_all(cls):
+        """
+        discover all the usb devices
+        """
         return usbinfo.usbinfo()
-    
+
     @classmethod
-    def get_filtered_list(cls, filters : dict[str, str], tty_only : bool=True):
+    def get_filtered_list(cls, filters: dict[str, str], tty_only: bool = True):
         """
         get a list of the usb properties for relevant devices
 
@@ -26,14 +34,14 @@ class USBs:
                     keep = False
                     break
             if tty_only:
-                if 'tty' not in connection["devname"]:
+                if "tty" not in connection["devname"]:
                     keep = False
             if keep:
                 filtered.append(connection)
         return filtered
-    
+
     @classmethod
-    def compute_serial_to_port_map(cls, filters : dict[str, str]):
+    def compute_serial_to_port_map(cls, filters: dict[str, str]):
         """
         returns a dictionary of the form {serial number -> dev port}
         """
@@ -41,32 +49,34 @@ class USBs:
 
         port_map = {}
         for connection in filt_list:
-            port_map[connection['iSerialNumber']] = connection['devname']
+            port_map[connection["iSerialNumber"]] = connection["devname"]
         return port_map
-    
+
     @classmethod
-    def plug_in_monitor(cls, usb_names : list = []):
+    def plug_in_monitor(cls, usb_names: list = None):
         """
-        live interaction script that will monitor which devices you plug in and save 
+        live interaction script that will monitor which devices you plug in and save
         their serial numbers in a list in order
         """
+        if usb_names is None:
+            usb_names = []
         prev_status = USBs.discover_all()
-        prev_serial_numbers = [d['iSerialNumber'] for d in prev_status]
+        prev_serial_numbers = [d["iSerialNumber"] for d in prev_status]
         new_serial_numbers = []
 
         i = 0
         while True:
             if usb_names == []:
-                prompt = 'Add in a USB and hit enter, or press something else and enter to exit'
+                prompt = "Add in a USB and hit enter, or press something else and enter to exit"
             else:
-                prompt = f'Plug in {usb_names[i]}'
+                prompt = f"Plug in {usb_names[i]}"
             s = input(prompt)
 
-            if s != '':
+            if s != "":
                 break
 
             cur_status = USBs.discover_all()
-            cur_serial_numbers = [d['iSerialNumber'] for d in cur_status]
+            cur_serial_numbers = [d["iSerialNumber"] for d in cur_status]
 
             res = list(set(cur_serial_numbers) - set(prev_serial_numbers))
             assert len(res) == 1
@@ -80,19 +90,14 @@ class USBs:
         return new_serial_numbers
 
 
-
-
 if __name__ == "__main__":
     ### test plug in monitor
 
-    usb_names = [
-        "MOTOR_0",
-        "MOTOR_1"
-    ]
-    new = USBs.plug_in_monitor(usb_names)
-    assert len(new) == len(usb_names)
+    usbs = ["MOTOR_0", "MOTOR_1"]
+    new = USBs.plug_in_monitor(usbs)
+    assert len(new) == len(usbs)
 
-    mapping = dict(zip(usb_names, new))
+    mapping = dict(zip(usbs, new))
 
     print(mapping)
     exit()
@@ -105,7 +110,7 @@ if __name__ == "__main__":
     print()
 
     filt = {
-        'iManufacturer' : 'Newport',
+        "iManufacturer": "Newport",
         # 'iSerialNumber' : 'A67BVBOJ'
     }
 
@@ -113,4 +118,3 @@ if __name__ == "__main__":
 
     m = USBs.compute_serial_to_port_map(filt)
     pprint(m)
-
