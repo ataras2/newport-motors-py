31b27217d3da948a569ee79f895b07ba47b83323
ataras2 <ataras2@gmail.com>
2023-06-07 13:57:18 +1000

    added documentation to function

--- newport_motors/GUI/TipTiltUI.py
+++ newport_motors/GUI/TipTiltUI.py
@@ -10,13 +10,21 @@ from newport_motors.Motors.motor import M100D
 
 
 class TipTiltUI:
+    @staticmethod
     def main(motor_key: str):
+        """
+        Create a UI block to control a tip/tilt motor, featuring a numeric input for the increment,
+        a numeric input for the absolute position of each axis, and a scatter plot of the current position
+
+        Parameters:
+            motor_key (str) : the key to use to store the motor in the session state
+        """
         st.header("Tip/Tilt motor")
         CustomNumeric.variable_increment(
             ["tip_tilt_u", "tip_tilt_v"],
             [
-                TipTiltUI.get_callback("tip_tilt_u", motor_key),
-                TipTiltUI.get_callback("tip_tilt_v", motor_key),
+                TipTiltUI.get_callback("tip_tilt_u", motor_key, M100D.AXES.U),
+                TipTiltUI.get_callback("tip_tilt_v", motor_key, M100D.AXES.V),
             ],
             st.session_state[motor_key].get_current_pos,
             main_bounds=M100D.HW_BOUNDS[M100D.AXES.V],
@@ -39,13 +47,16 @@ class TipTiltUI:
 
         st.write(fig)
 
-    def get_callback(source: str, motor_key: str) -> callable:
-        if source[-1] == "u":
-            axis = M100D.AXES.U
-        elif source[-1] == "v":
-            axis = M100D.AXES.V
-        else:
-            raise NotImplementedError()
+    @staticmethod
+    def get_callback(source: str, motor_key: str, axis: M100D.AXES) -> callable:
+        """
+        Return a callback function to set the absolute position of the motor in a given axis
+
+        Parameters:
+            source (str) : the key to use to store the value to move to in the session state
+            motor_key (str) : the key to use to store the motor in the session state
+            axis (M100D.AXES) : the axis to set
+        """
 
         def fn():
             logging.info(
